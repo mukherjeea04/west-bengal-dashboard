1bb20c18ac65f94017f4093aa724f84564379eb0
Akash Mukherjee <mukherjeea04@gmail.com>
2026-08-28 18:18:17 +0530

    Adjust map zoom

--- app.py
+++ app.py
@@ -461,7 +461,7 @@ def update_map(selected_year, selected_indicator):
             "lon": center_lon
         },
 
-        zoom=5,
+        zoom=5.2,
 
         labels={
             "value": selected_indicator
